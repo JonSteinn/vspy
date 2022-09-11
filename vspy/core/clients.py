28ac64d8d3e654ca1ee0d01f7a0f59614bee8572
JonSteinn <jonsteinn@gmail.com>
2022-09-11 17:15:01 +0000

    drop protocol for 3.7

--- vspy/core/clients.py
+++ vspy/core/clients.py
@@ -1,24 +1,11 @@
-import abc
 import asyncio
-from typing import Awaitable, Dict, Iterable, List, Optional, Protocol, Tuple
+from typing import Awaitable, Dict, Iterable, List, Tuple
 
 import httpx
 from bs4 import BeautifulSoup
 
 
-class Client(Protocol):
-    """Base client."""
-
-    @abc.abstractmethod
-    async def get_json(self, url: str) -> dict:
-        """Get request promise to the url, deserialized as a json."""
-
-    @abc.abstractmethod
-    async def get(self, url: str) -> str:
-        """Get request promise to the url, raw content as string."""
-
-
-class AsyncClient(Client):
+class AsyncClient:
     """Async client to make multiple requests."""
 
     def __init__(self) -> None:
@@ -30,17 +17,19 @@ class AsyncClient(Client):
         return res
 
     async def get_json(self, url: str) -> dict:
+        """Get request promise to the url, deserialized as a json."""
         json: dict = (await self._get(url)).json()
         return json
 
     async def get(self, url: str) -> str:
+        """Get request promise to the url, raw content as string."""
         return (await self._get(url)).text
 
 
 class PyPiClient:
     """A client that fetched PyPi package versions."""
 
-    def __init__(self, client: Client) -> None:
+    def __init__(self, client: AsyncClient) -> None:
         self._client = client
 
     async def get_version(self, package: str) -> Tuple[str, str]:
@@ -59,7 +48,7 @@ class PyPiClient:
 class PythonVersionClient:
     """Client for fetching active python versions."""
 
-    def __init__(self, client: Client) -> None:
+    def __init__(self, client: AsyncClient) -> None:
         self._client = client
 
     async def active_python3_version(self) -> List[str]:
@@ -81,11 +70,10 @@ class PythonVersionClient:
 
 
 async def fetch_all_requests_data(
-    packages: Iterable[str], client: Optional[Client] = None
+    packages: Iterable[str],
 ) -> Tuple[Dict[str, str], List[str]]:
     """Perform all the client calls."""
-    if not client:
-        client = AsyncClient()
+    client = AsyncClient()
     pypi_cli = PyPiClient(client)
     py_cli = PythonVersionClient(client)
     res = await asyncio.gather(
